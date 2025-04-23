bf5d13498672291421d9b21b7b47d39e275ab4ff
gitName <bartk@list.ru>
2025-04-23 14:52:36 +0300

    chore: centered tabs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
                         <li class="me-2">
                             <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
                                 aria-current="page">Tab 1</a>
