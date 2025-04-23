c5f29142eadb5d531e2d0e3f0fc8d203e96b03fd
gitName <bartk@list.ru>
2025-04-23 19:04:53 +0300

    refactor: constraint the post content to 15 words or less

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
                                     {{$post->title}}
                                 </h5>
                             </a>
-                            <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$post->content}}</div>
+                            <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{Str::words($post->content, 15)}}</div>
                             <a href="#"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Read more
