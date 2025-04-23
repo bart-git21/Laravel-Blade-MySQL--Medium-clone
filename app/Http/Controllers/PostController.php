7b4b279bf225a08a8a8936bbd3afe3721474d465
gitName <bartk@list.ru>
2025-04-23 16:04:40 +0300

    chore: define PostController index()

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        // 
+        $categories = Category::get();
+        return view('dashboard', compact('categories'));
     }
 
     /**
