8a82a2e8014bf06c631d3ec419116d3c2fb05bd1
gitName <bartk@list.ru>
2025-04-25 21:30:17 +0300

    fix: use closing tag

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,6 +9,7 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
+            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
