f477b398fe3099df83bf32bbba0d0890a77a7dfa
gitName <bartk@list.ru>
2025-04-26 17:58:02 +0300

    chore: add category input inside post creating form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -18,6 +18,18 @@
                             :value="old('title')" required autofocus />
                         <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
+                    <!-- Category -->
+                    <div class="mt-4">
+                        <x-input-label for="category_id" :value="__('Category')" />
+                        <select id="category_id" name="category_id" class="border-gray-300 focus:border-indigo-500
+focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full">
+                            <option value="">Select a category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                    </div>
                     <!-- Content -->
                     <div class="mt-4">
                         <x-input-label for="content" :value="__('Content')" />
