d9e54696395d7369e178538c1a871bb3c107227f
gitName <bartk@list.ru>
2025-04-26 17:57:33 +0300

    refactor: use multipart/form-data type for form which includes file input

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
     <div class="py-4">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
-                <form action="{{ route('post.store') }}" method="POST">
+                <form action="{{ route('post.store') }}" enctype="multipart/form-data" method="POST">
                     @csrf
                     <!-- Image file -->
                     <div>
