9fa35e24c00bd26df8fd704ffa4afd1b14419d67
gitName <bartk@list.ru>
2025-04-26 14:35:57 +0300

    chore: add file input inside post creating form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,8 +3,15 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
                 <form action="{{ route('post.store') }}" method="POST">
-                    <!-- Title -->
+                    <!-- Image file -->
                     <div>
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
+                            :value="old('image')" required />
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+                    <!-- Title -->
+                    <div class="mt-4">
                         <x-input-label for="title" :value="__('Title')" />
                         <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
                             :value="old('title')" required autofocus />
