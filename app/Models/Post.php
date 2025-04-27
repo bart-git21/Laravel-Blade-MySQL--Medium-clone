f81773dd4243034a5d5834c54f63180557c6a957
gitName <bartk@list.ru>
2025-04-27 21:25:16 +0300

    chore: define imageUrl() function to get path to the stored user avatar image

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -22,5 +23,13 @@ class Post extends Model
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function imageUrl() {
+        if ($this->image) {
+            return Storage::url($this->image);
+        }
+
+        return null;
     }
 }
