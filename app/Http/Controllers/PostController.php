1fbabc1e5921dfb4e7fec411cc07e81c5c681d5b
gitName <bartk@list.ru>
2025-04-27 16:15:14 +0300

    chore: define PostController show() function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,9 +58,9 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(string $username, Post $post)
     {
-        //
+        return 'title: ' .$post->title;
     }
 
     /**
