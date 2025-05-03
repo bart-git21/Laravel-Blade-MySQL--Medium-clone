d48236d426bfa1381f293e540ce37fec3e2062d1
gitName <bartk@list.ru>
2025-05-03 21:08:10 +0300

    chore: create jobs array

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,26 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '10000000',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '5000000',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '2000000',
+    ]
+];
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('dashboard');
