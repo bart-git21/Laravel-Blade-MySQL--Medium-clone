5d949d8709801750b0dbb239f7bcb6d2cd2e026a
gitName <bartk@list.ru>
2025-05-03 21:57:43 +0300

    chore: create jobs blade page

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot:header>Jobs listing</x-slot:header>
+    <ul>
+        @forelse ($jobs as $job)
+            <li>
+                <a href="/jobs">{{ $job['title'] }} - {{ $job['salary'] }}</a>
+            </li>
+        @empty
+            <p>List is empty!</p>
+        @endforelse
+    </ul>
+</x-app-layout>
