6bd6e9be5a0b05fb4858de2698f30b97b2568d80
gitName <bartk@list.ru>
2025-05-04 14:25:56 +0300

    refactor: merge /jobs and /jobs/{id} endpoints

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,21 @@ use App\Http\Controllers\ProfileController;
 use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('dashboard');
     Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
     Route::post('/post/create', [PostController::class, 'store'])->name('post.store');
     Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])->name('post.show');
+    Route::get('/jobs', function (Request $request)  {
+        if ($request->id) {
+            $id = $request->input('id');
+            return view('job', ['job' => Arr::first(Job::all(), fn($job) => $job['id'] === intval($id))]);
+        }
+    
+        return view('jobs', ['jobs' => Job::all()]);
+    })->name('jobs.all');
 });
 
 Route::middleware('auth')->group(function () {
@@ -26,10 +35,5 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/jobs', fn() => view('jobs', ['jobs' => Job::all()]))->name('jobs');
-Route::get('/jobs/{id}', function ($id) {
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] === intval($id));
-    return view('job', ['job' => $job]);
-})->name('job');
 
 require __DIR__ . '/auth.php';
