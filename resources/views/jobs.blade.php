7c6593e490d891b16c3ae1d77652be4b315c7660
gitName <bartk@list.ru>
2025-05-04 14:22:17 +0300

    refactor: use form to redirect into specific job page

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,13 @@
     <ul>
         @forelse ($jobs as $job)
             <li>
-                <a href="/jobs/{{ $job['id'] }}">{{ $job['title'] }} - {{ $job['salary'] }}</a>
+                <form class="max-w-sm mx-auto" action="/jobs" method="GET">
+                    <div class="mb-5">
+                        <input type="text" name="id" hidden value="{{ $job['id'] }}" />
+                    </div>
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ $job['title'] }} - {{ $job['salary'] }}</button>
+                </form>
             </li>
         @empty
             <p>List is empty!</p>
